d52a74025b958f3e01eb5a6a184cb23beebf682d
YadaYuki <yada.yuki@fuji.waseda.jp>
2022-03-16 14:46:46 +0900

    Add Dot

--- utils/utils.go
+++ utils/utils.go
@@ -1,11 +1,12 @@
 package utils
 
 import (
+	"fmt"
+
 	"github.com/vorduin/nune"
 )
 
-// TODO: correspond to 2 dimensional tensor
-func Equal[T nune.Number](a, b nune.Tensor[T]) bool {
+func Equal1D[T nune.Number](a, b nune.Tensor[T]) bool {
 	if a.Size(0) != b.Size(0) {
 		return false
 	}
@@ -17,13 +18,53 @@ func Equal[T nune.Number](a, b nune.Tensor[T]) bool {
 	return true
 }
 
+func Equal2D[T nune.Number](a, b nune.Tensor[T]) bool {
+	if a.Shape()[0] != b.Shape()[0] || a.Shape()[1] != b.Shape()[1] {
+		return false
+	}
+	I := a.Shape()[0]
+	J := a.Shape()[1]
+	for i := 0; i < I; i++ {
+		for j := 0; j < J; j++ {
+			if a.Index(i, j).Scalar() != b.Index(i, j).Scalar() {
+				return false
+			}
+		}
+	}
+	return true
+}
+
 func Add[T nune.Number](a, b nune.Tensor[T]) nune.Tensor[T] {
 	if a.Size(0) != b.Size(0) {
 		panic("Tensors must have the same size")
 	}
-	add := make([]T, a.Size(0))
+	result := make([]T, a.Size(0))
 	for i := 0; i < a.Size(0); i++ {
-		add[i] = a.Index(i).Scalar() + b.Index(i).Scalar()
+		result[i] = a.Index(i).Scalar() + b.Index(i).Scalar()
 	}
-	return nune.From[T](add)
+	return nune.FromBuffer(result)
+}
+
+// dot 2D tensor & 2D tensor a * b
+func Dot[T nune.Number](a, b nune.Tensor[T]) nune.Tensor[T] {
+	Ia := a.Size(0)
+	Ja := a.Size(1)
+	Ib := b.Size(0)
+	Jb := b.Size(1)
+	if Ja != Ib {
+		panic(fmt.Sprint("a.Size(1),b.Size(0) must have the same size", a.Shape(), b.Shape()))
+	}
+	result := nune.Zeros[T](Ia, Jb)
+	for ia := 0; ia < Ia; ia++ {
+		for jb := 0; jb < Jb; jb++ {
+			sum := T(0)
+			for j := 0; j < Ja; j++ {
+				ib := j
+				ja := j
+				sum += a.Index(ia, ja).Scalar() * b.Index(ib, jb).Scalar()
+			}
+			result.Index(ia, jb).Ravel()[0] = sum
+		}
+	}
+	return result
 }
--- utils/utils_test.go
+++ utils/utils_test.go
@@ -11,7 +11,38 @@ func TestAdd(t *testing.T) {
 	b := nune.Range[int](0, 10, 1)
 	expected := nune.Range[int](0, 20, 2)
 	c := Add(a, b)
-	if !Equal(c, expected) {
+	if !Equal1D(c, expected) {
 		t.Errorf("Expected %v, got %v", expected, c)
+	}
+}
+
+func TestDot(t *testing.T) {
+	testCases := []struct {
+		a        nune.Tensor[int]
+		b        nune.Tensor[int]
+		expected nune.Tensor[int]
+	}{
+		{a: nune.Range[int](0, 9, 1).Reshape(3, 3),
+			b: nune.Range[int](0, 9, 1).Reshape(3, 3),
+			expected: nune.FromBuffer([]int{
+				15, 18, 21,
+				42, 54, 66,
+				69, 90, 111,
+			}).Reshape(3, 3),
+		},
+		{a: nune.Range[int](0, 12, 1).Reshape(3, 4),
+			b: nune.Range[int](0, 12, 1).Reshape(4, 3),
+			expected: nune.FromBuffer([]int{
+				42, 48, 54,
+				114, 136, 158,
+				186, 224, 262,
+			}).Reshape(3, 3),
+		},
+	}
+	for _, tt := range testCases {
+		c := Dot(tt.a, tt.b)
+		if !Equal2D(c, tt.expected) {
+			t.Errorf("Expected %v, got %v", tt.expected, c)
+		}
 	}
 }
