b14ece5e9b7529286c3785837fce30381b77cbdc
YadaYuki <yada.yuki@fuji.waseda.jp>
2022-03-16 12:51:11 +0900

    Add Add function

--- utils/utils.go
+++ utils/utils.go
@@ -1,4 +1,29 @@
 package utils
 
-import 
+import (
+	"github.com/vorduin/nune"
+)
 
+// TODO: correspond to 2 dimensional tensor
+func Equal[T nune.Number](a, b nune.Tensor[T]) bool {
+	if a.Size(0) != b.Size(0) {
+		return false
+	}
+	for i := 0; i < a.Size(0); i++ {
+		if a.Index(i).Scalar() != b.Index(i).Scalar() {
+			return false
+		}
+	}
+	return true
+}
+
+func Add[T nune.Number](a, b nune.Tensor[T]) nune.Tensor[T] {
+	if a.Size(0) != b.Size(0) {
+		panic("Tensors must have the same size")
+	}
+	add := make([]T, a.Size(0))
+	for i := 0; i < a.Size(0); i++ {
+		add[i] = a.Index(i).Scalar() + b.Index(i).Scalar()
+	}
+	return nune.From[T](add)
+}
--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,1 +1,17 @@
 package utils
+
+import (
+	"testing"
+
+	"github.com/vorduin/nune"
+)
+
+func TestAdd(t *testing.T) {
+	a := nune.Range[int](0, 10, 1)
+	b := nune.Range[int](0, 10, 1)
+	expected := nune.Range[int](0, 20, 2)
+	c := Add(a, b)
+	if !Equal(c, expected) {
+		t.Errorf("Expected %v, got %v", expected, c)
+	}
+}
