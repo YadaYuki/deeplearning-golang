0ed49e6d20e91395d966bea5cfd05b1f1302a027
YadaYuki <yada.yuki@fuji.waseda.jp>
2022-03-17 14:56:51 +0900

    Add TwoLayerNet

---
+++ model/model_test.go
@@ -0,0 +1,47 @@
+package model
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/vorduin/nune"
+)
+
+func TestPredict(t *testing.T) {
+	weightInitilizer := func() float64 {
+		return rand.Float64()
+	}
+	batchSize := 10
+	inputDim := 15
+	hiddenDim := 5
+	outDim := 10
+	net := NewTwoLayerNet(inputDim, hiddenDim, outDim, weightInitilizer)
+	out := net.Predict(nune.Ones[float64](batchSize, inputDim))
+	if out.Shape()[0] != batchSize || out.Shape()[1] != outDim {
+		t.Errorf("out.Shape() != []int{batchSize, outDim}")
+	}
+	for i := 0; i < batchSize; i++ {
+		for j := 0; j < outDim; j++ {
+			if out.Index(i, j).Scalar() > 1.0 {
+				t.Errorf("out.Index(%d, %d).Scalar() > 1.0", i, j)
+			}
+		}
+	}
+}
+
+func TestTrainStep(t *testing.T) {
+	weightInitilizer := func() float64 {
+		return rand.Float64()
+	}
+	batchSize := 10
+	inputDim := 15
+	hiddenDim := 5
+	outDim := 10
+	net := NewTwoLayerNet(inputDim, hiddenDim, outDim, weightInitilizer)
+	x := nune.Ones[float64](batchSize, inputDim)
+	tt := nune.FromBuffer([]float64{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0}).Reshape(batchSize, outDim)
+	loss := net.TrainStep(x, tt)
+	if loss == 0.0 {
+		t.Errorf("loss != 0.0")
+	}
+}
