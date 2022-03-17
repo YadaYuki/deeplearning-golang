54e6597de11b20fff26bd9af07e70b15e13a5e2a
YadaYuki <yada.yuki@fuji.waseda.jp>
2022-03-17 10:42:23 +0900

    Add Affine

--- layers/affine.go
+++ layers/affine.go
@@ -26,11 +26,11 @@ func (affine *Affine[T]) Forward(X nune.Tensor[T]) nune.Tensor[T] {
 	return Y
 }
 
-func (affine *Affine[T]) Backward(dy nune.Tensor[T]) (nune.Tensor[T], nune.Tensor[T], nune.Tensor[T]) {
-	dx := utils.Dot(dy, utils.Transpose(*affine.W))
-	dW := utils.Dot(utils.Transpose(affine.xM), dy)
-	dB := utils.Sum(dy)
-	*affine.W = utils.Add(*affine.W, dW.Mul(-1.0))
-	*affine.B = utils.Add(*affine.B, dB.Mul(-1.0))
+func (affine *Affine[T]) Backward(dy nune.Tensor[T]) (dx nune.Tensor[T], dW nune.Tensor[T], dB nune.Tensor[T]) {
+	dx = utils.Dot(dy, utils.Transpose(*affine.W))
+	dW = utils.Dot(utils.Transpose(affine.xM), dy)
+	dB = utils.Sum(dy)
+	*affine.W = utils.Add(*affine.W, dW.Clone().Mul(-1.0)) // dW自体も書き換えられてしまうため、Cloneしておく
+	*affine.B = utils.Add(*affine.B, dB.Clone().Mul(-1.0)) // dB自体も書き換えられてしまうため、Cloneしておく
 	return dx, dW, dB
 }
--- layers/affine_test.go
+++ layers/affine_test.go
@@ -24,9 +24,28 @@ func TestAffineBackward(t *testing.T) {
 	B := nune.FromBuffer([]int{1, 1})
 	X := nune.FromBuffer([]int{1, 2, 3, 1, 2, 3, 1, 2, 3}).Reshape(3, 3)
 	affine := NewAffine(&W, &B)
-	expected := nune.FromBuffer([]int{23, 29, 23, 29, 23, 29}).Reshape(3, 2)
-	Y := affine.Forward(X)
-	if !utils.Equal2D(Y, expected, func(a, b int) bool { return a == b }) {
-		t.Errorf("Expected %v, got %v", expected, Y)
+	affine.Forward(X)
+	dx, dW, dB := affine.Backward(nune.FromBuffer([]int{1, 2, 3, 1, 2, 3}).Reshape(3, 2))
+
+	expected_dx := nune.FromBuffer([]int{5, 11, 17, 5, 13, 21, 8, 18, 28}).Reshape(3, 3)
+	expected_dW := nune.FromBuffer([]int{6, 6, 12, 12, 18, 18}).Reshape(3, 2)
+	expected_dB := nune.FromBuffer([]int{6, 6})
+	expected_W := nune.FromBuffer([]int{-5, -4, -9, -8, -13, -12}).Reshape(3, 2)
+	expected_B := nune.FromBuffer([]int{-5, -5})
+
+	if !utils.Equal2D(dx, expected_dx, func(a, b int) bool { return a == b }) {
+		t.Errorf("Expected %v, got %v", expected_dx, dx)
+	}
+	if !utils.Equal2D(dW, expected_dW, func(a, b int) bool { return a == b }) {
+		t.Errorf("Expected %v, got %v", expected_dW, dW)
+	}
+	if !utils.Equal1D(dB, expected_dB, func(a, b int) bool { return a == b }) {
+		t.Errorf("Expected %v, got %v", expected_dB, dB)
+	}
+	if !utils.Equal2D(*affine.W, expected_W, func(a, b int) bool { return a == b }) {
+		t.Errorf("Expected %v, got %v", expected_W, affine.W)
+	}
+	if !utils.Equal1D(*affine.B, expected_B, func(a, b int) bool { return a == b }) {
+		t.Errorf("Expected %v, got %v", expected_B, affine.B)
 	}
 }
